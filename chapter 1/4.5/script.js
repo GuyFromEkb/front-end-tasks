44b1abbabeec42cd1f04a7ae932bff3ebd7b7926
Sergey_nasty <zaebalacs@gmail.com>
2022-06-20 02:21:25 +0500

    5-3

--- chapter 1/4.5/script.js
+++ chapter 1/4.5/script.js
@@ -58,3 +58,36 @@ calculator.read();
 
 alert("Sum=" + calculator.sum());
 alert("Mul=" + calculator.mul());
+
+
+/* Напишите функцию-конструктор Accumulator(startingValue).
+
+Объект, который она создаёт, должен уметь следующее:
+
+Хранить «текущее значение» в свойстве value. Начальное значение устанавливается в аргументе конструктора startingValue.
+Метод read() использует prompt для получения числа и прибавляет его к свойству value.
+Таким образом, свойство value является текущей суммой всего, что ввёл пользователь при вызовах метода read(), с учётом начального значения startingValue.
+
+Ниже вы можете посмотреть работу кода:
+
+let accumulator = new Accumulator(1); // начальное значение 1
+
+accumulator.read(); // прибавит ввод prompt к текущему значению
+accumulator.read(); // прибавит ввод prompt к текущему значению
+
+alert(accumulator.value); // выведет сумму этих значений */
+
+function Accumulator(startingValue) {
+    this.value = startingValue;
+
+    this.read = function() {
+        const numb = +prompt("введите значение:");
+        this.value += numb;
+        return this;
+    };
+}
+
+const acc = new Accumulator(2);
+
+acc.read().read();
+alert(acc.value);
