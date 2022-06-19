56c175135b92529fdd21ff1c2a3042d060902f84
Sergey_nasty <zaebalacs@gmail.com>
2022-06-19 20:07:34 +0500

    1-2

--- chapter 1/4.1/script.js
+++ chapter 1/4.1/script.js
@@ -14,3 +14,18 @@ user['surname'] = 'Smitch';
 user['name'] = 'Pete';
 
 delete user['name'];
+
+
+/* Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+Должно работать так:
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) ); // false */
+
+function isEmpty(obj) {
+
+    return Object.keys(obj).length == 0 ? true : false;
+}
+const foo = {};
+console.log(isEmpty(user));
