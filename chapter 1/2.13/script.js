6ac212a2f864a42d79ab1869a1198d7bde1300a0
Sergey_nasty <zaebalacs@gmail.com>
2022-06-19 06:27:45 +0500

    13-5

--- chapter 1/2.13/script.js
+++ chapter 1/2.13/script.js
@@ -44,3 +44,16 @@ for (let i = 1; i < 10; i++) {
         console.log(i)
     else continue;
 }
+
+
+// Перепишите код, заменив цикл for на while, без изменения поведения цикла.
+/* 
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+} */
+
+let i = 0;
+while (i < 3) {
+    alert(`number ${i}!`);
+    i++;
+}
