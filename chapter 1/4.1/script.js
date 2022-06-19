42a1b06343dc1a9ce4fd5588828a5ae0ef90b575
Sergey_nasty <zaebalacs@gmail.com>
2022-06-19 20:19:46 +0500

    1-3,4

--- chapter 1/4.1/script.js
+++ chapter 1/4.1/script.js
@@ -24,8 +24,81 @@ schedule["8:30"] = "get up";
 alert( isEmpty(schedule) ); // false */
 
 function isEmpty(obj) {
-
     return Object.keys(obj).length == 0 ? true : false;
 }
-const foo = {};
-console.log(isEmpty(user));
+
+/* Можно ли изменить объект, объявленный с помощью const? Как вы думаете?
+
+const user = {
+  name: "John"
+};
+
+// это будет работать?
+user.name = "Pete"; */
+
+константе присваевается ссылка на объект в памяти, изменение свойсвта будет работать, т.к.сама константа с сылкой не меняется
+
+
+/* У нас есть объект, в котором хранятся зарплаты нашей команды:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. Должно получиться 390.
+
+Если объект salaries пуст, то результат должен быть 0. */
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+};
+let sum = 0;
+
+for (const key in salaries) {
+    sum += salaries[key];
+}
+
+/* Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+
+Например:
+
+    // до вызова функции
+    let menu = {
+        width: 200,
+        height: 300,
+        title: "My menu"
+    };
+
+multiplyNumeric(menu);
+
+// после вызова функции
+menu = {
+    width: 400,
+    height: 600,
+    title: "My menu"
+};
+Обратите внимание, что multiplyNumeric не нужно ничего возвращать.Следует напрямую изменять объект.
+
+P.S.Используйте typeof для проверки, что значение свойства числовое. */
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+console.log(menu);
+
+function multiplyNumeric(obj) {
+
+    for (const key in obj) {
+        if (typeof(obj[key]) == "number") {
+            obj[key] *= 2;
+        }
+    }
+}
+multiplyNumeric(menu);
+console.log(menu);
