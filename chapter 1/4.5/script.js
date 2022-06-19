739f9b85b7dd7508e0b9aa908b710732731d72a1
Sergey_nasty <zaebalacs@gmail.com>
2022-06-20 02:13:43 +0500

    5-2

--- chapter 1/4.5/script.js
+++ chapter 1/4.5/script.js
@@ -21,3 +21,40 @@ function A() { return human; }
 function B() { return human; }
 
 console.log(A() == B());
+
+
+/* Создайте функцию-конструктор Calculator, который создаёт объекты с тремя методами:
+
+read() запрашивает два значения при помощи prompt и сохраняет их значение в свойствах объекта.
+sum() возвращает сумму введённых свойств.
+mul() возвращает произведение введённых свойств.
+Например:
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() ); */
+
+
+function Calculator() {
+
+    this.read = function(operandA, operandB) {
+        this['operandA'] = +prompt('Значение1', 0);
+        this['operandB'] = +prompt('Значение2', 0);
+    };
+
+    this.sum = function() {
+        return this['operandA'] + this['operandB'];
+    };
+
+    this.mul = function() {
+        return this['operandA'] * this['operandB'];
+    };
+}
+
+const calculator = new Calculator();
+calculator.read();
+
+alert("Sum=" + calculator.sum());
+alert("Mul=" + calculator.mul());
