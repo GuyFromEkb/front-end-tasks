c0380688ead2039635eda4d23dfc1bf239656f87
Sergey <graygmai1@gmail.com>
2022-06-20 23:03:52 +0500

    5-6

--- chapter 1/5.5/script.js
+++ chapter 1/5.5/script.js
@@ -123,9 +123,77 @@ function copySorted(arr) {
 
 }
 
-let arr1 = ["HTML", "JavaScript", "CSS"];
 
-arr2 = copySorted(arr1);
+/* Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
 
-console.log(arr1);
-console.log(arr2);
+Задание состоит из двух частей.
+
+Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами)
+и возвращает результат. Метод должен понимать плюс + и минус -.
+
+Пример использования:
+
+let calc = new Calculator;
+
+alert( calc.calculate("3 + 7") ); // 10
+Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
+
+Например, давайте добавим умножение *, деление / и возведение в степень **:
+
+let powerCalc = new Calculator;
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+
+let result = powerCalc.calculate("2 ** 3");
+alert( result ); // 8
+Для этой задачи не нужны скобки или сложные выражения.
+Числа и оператор разделены ровно одним пробелом.
+Не лишним будет добавить обработку ошибок. */
+
+
+function Calculator() {
+
+    this.calculate = function(str) {
+        const buffArr = str.split(" ");
+
+        const operandA = +buffArr[0];
+        const operandB = +buffArr[2];
+        const sign = buffArr[1];
+
+        if (!(sign in this.actions) || isNaN(operandA) || isNaN(operandB)) {
+            return NaN;
+        }
+
+
+        return this.actions[sign](operandA, operandB);
+
+
+
+    };
+
+
+    this.actions = {
+        "+": (operandA, operandB) => {
+            return operandA + operandB;
+        },
+        "-": (operandA, operandB) => {
+            return operandA - operandB;
+        },
+    };
+
+
+
+    this.addMethod = function(sign, func) {
+        this.actions[sign] = func;
+    };
+
+}
+
+let powerCalc = new Calculator();
+
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+
+console.log(powerCalc.calculate("2 ** 3"));
