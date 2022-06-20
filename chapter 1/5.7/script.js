965c0563f651bff08722f91595b6ad3c62adc729
Sergey <graygmai1@gmail.com>
2022-06-21 01:33:55 +0500

    7-1

--- chapter 1/5.7/script.js
+++ chapter 1/5.7/script.js
@@ -31,3 +31,43 @@ function unique(arr) {
 }
 
 console.log(unique(values));
+
+/* 
+Анаграммы – это слова, у которых те же буквы в том же количестве, но они располагаются в другом порядке.
+Например:
+
+nap - pan
+ear - are - era
+cheaters - hectares - teachers
+Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
+
+Например:
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert( aclean(arr) ); // "nap,teachers,ear" или "PAN,cheaters,era"
+Из каждой группы анаграмм должно остаться только одно слово, не важно какое. */
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+
+function aclean(arr) {
+    const arrSet = new Set();
+
+    for (const item of arr) {
+        const buffitem = item.toLowerCase().split("").sort().join("");
+        arrSet.add(buffitem);
+    }
+
+    return Array.from(arrSet);
+
+}
+
+
+
+console.log(arrSet)
+const tstSet = new Set();
+tstSet.add(tst[1]);
+tstSet.add(tst[2]);
+
+console.log(tstSet)
