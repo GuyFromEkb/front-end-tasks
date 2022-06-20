cbbf0781413b006b574e9946f78e5ed0613f065b
Sergey <graygmai1@gmail.com>
2022-06-20 21:25:37 +0500

    5-2

--- chapter 1/5.5/script.js
+++ chapter 1/5.5/script.js
@@ -33,3 +33,33 @@ function camelize(property) {
 }
 
 camelize("-webkit-transition");
+
+/* 
+Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
+
+Функция должна возвращать новый массив и не изменять исходный.
+
+Например:
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (совпадающие значения)
+
+alert( arr ); // 5,3,8,1 (без изменений) */
+
+function filterRange(arr, value1, value2) {
+
+    const newArr = arr.filter((item) => {
+        return (item >= value1 && item <= value2);
+    });
+
+    return newArr;
+}
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+
+
+console.log(filtered(testarr, 1, 4));
