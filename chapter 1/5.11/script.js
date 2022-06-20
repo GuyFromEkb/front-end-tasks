8c3fde4083982293e9f7de66b8fce33377cf5d55
Sergey <graygmai1@gmail.com>
2022-06-21 03:05:39 +0500

    11-5

--- chapter 1/5.11/script.js
+++ chapter 1/5.11/script.js
@@ -73,3 +73,24 @@ let date2 = new Date(2015, 0, 2);
 console.log(getDateAgo(date2, 1)); // 1, (1 Jan 2015)
 console.log(getDateAgo(date2, 2)); // 31, (31 Dec 2014)
 console.log(getDateAgo(date2, 365)); // 2, (2 Jan 2014)
+
+
+
+/* 
+Напишите функцию getLastDayOfMonth(year, month), возвращающую последнее число месяца. Иногда это 30, 31 или даже февральские 28/29.
+
+Параметры:
+
+year – год из четырёх цифр, например, 2012.
+month – месяц от 0 до 11.
+К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
+
+ */
+
+function getLastDayOfMonth(year, month) {
+    let date1 = new Date(year, month);
+    let date2 = new Date(year, month + 1);
+    return (date2 - date1) / 1000 / 3600 / 24;
+}
+
+console.log(getLastDayOfMonth(2012, 1))
