32128805080f0ded3f125927a17bf27291548245
Sergey <graygmai1@gmail.com>
2022-06-21 01:16:45 +0500

    5.7

---
+++ chapter 1/5.7/script.js
@@ -0,0 +1,33 @@
+"use strict";
+/* 
+Допустим, у нас есть массив arr.
+Создайте функцию unique(arr), которая вернёт массив уникальных, не повторяющихся значений массива arr.
+
+Например:
+
+function unique(arr) {
+
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert( unique(values) ); // Hare,Krishna,:-O
+
+P.S. Здесь мы используем строки, но значения могут быть любого типа.
+P.P.S. Используйте Set для хранения уникальных значений. */
+
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+function unique(arr) {
+
+    const tst = new Set(arr);
+
+    return Array.from(tst);
+}
+
+console.log(unique(values));
