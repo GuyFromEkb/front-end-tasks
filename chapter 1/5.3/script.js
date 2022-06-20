f54dbfbf30a2a31ad40a5d37eca714537a364642
Sergey_nasty <zaebalacs@gmail.com>
2022-06-20 06:13:43 +0500

    3-2

--- chapter 1/5.3/script.js
+++ chapter 1/5.3/script.js
@@ -8,3 +8,19 @@ function capitalize(word) {
     return word[0].toUpperCase() + word.slice(1);
 }
 console.log(capitalize("adsa"))
+
+/* Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+
+Функция должна быть нечувствительна к регистру:
+
+checkSpam('buy ViAgRA now') == true
+checkSpam('free xxxxx') == true
+checkSpam("innocent rabbit") == false */
+
+function checkSpam(string) {
+
+    if (string.toLowerCase().includes("viagra") || string.toLowerCase().includes("xxx")) {
+        return true;
+    }
+    return false;
+}
