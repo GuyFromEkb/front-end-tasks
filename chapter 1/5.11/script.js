5251e01f8f963a44a63cae3ccb1111d9f8de9e58
Sergey <graygmai1@gmail.com>
2022-06-21 02:49:09 +0500

    11-2

--- chapter 1/5.11/script.js
+++ chapter 1/5.11/script.js
@@ -5,5 +5,22 @@
 Для вывода используйте alert. */
 
 const dataFeb = new Date(2012, 1, 20, 3, 12);
-// const dataFeb = new Date();
+
 alert(dataFeb);
+
+
+/* Напишите функцию getWeekDay(date), показывающую день недели в коротком формате: «ПН», «ВТ», «СР», «ЧТ», «ПТ», «СБ», «ВС».
+Например:
+
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+alert( getWeekDay(date) );        // нужно вывести "ВТ"
+ */
+
+function getWeekDay(date) {
+    const days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+
+    return days[date.getDay()];
+}
+
+let date = new Date(2014, 0, 3); // 3 января 2014 года
+console.log(getWeekDay(date)); // ПТ
