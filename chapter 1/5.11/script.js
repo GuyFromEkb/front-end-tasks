2ff1ff6a684e295b70bf86acebe733208916416f
Sergey <graygmai1@gmail.com>
2022-06-21 03:12:17 +0500

    11-7

--- chapter 1/5.11/script.js
+++ chapter 1/5.11/script.js
@@ -110,3 +110,22 @@ function getSecondsToday() {
 
     return date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
 }
+
+
+
+/* Создайте функцию getSecondsToTomorrow(), возвращающую количество секунд до завтрашней даты.
+
+Например, если сейчас 23: 00, то:
+
+    getSecondsToTomorrow() == 3600
+P.S.Функция должна работать в любой день, т.е.в ней не должно быть конкретного значения сегодняшней даты.
+ */
+
+
+function getSecondsToTomorrow() {
+
+    const date = new Date();
+
+    return 24 * 3600 - (date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds());
+
+}
