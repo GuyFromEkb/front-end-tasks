60f2ed30045aafe1d352bc460b3d405218b2a9ad
Sergey <graygmai1@gmail.com>
2022-06-21 03:47:57 +0500

    11-8

--- chapter 1/5.11/script.js
+++ chapter 1/5.11/script.js
@@ -129,3 +129,55 @@ function getSecondsToTomorrow() {
     return 24 * 3600 - (date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds());
 
 }
+
+
+
+/* Напишите функцию formatDate(date), форматирующую date по следующему принципу:
+
+Если спустя date прошло менее 1 секунды, вывести "прямо сейчас".
+В противном случае, если с date прошло меньше 1 минуты, вывести "n сек. назад".
+В противном случае, если меньше часа, вывести "m мин. назад".
+В противном случае, полная дата в формате "DD.MM.YY HH:mm". А именно: "день.месяц.год часы:минуты", всё в виде двух цифр, т.е. 31.12.16 10:00.
+Например:
+
+alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
+
+alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
+
+alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
+
+// вчерашняя дата вроде 31.12.2016, 20:00
+alert( formatDate(new Date(new Date - 86400 * 1000)) ); */
+
+
+
+function formatDate(date) {
+
+    const now = new Date();
+    const n = now - date;
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = `${date.getFullYear()}`.slice(2);
+    let hours = date.getHours();
+    let mins = date.getMinutes();
+
+    if (`${day}`.length < 2)
+        day = `0${day}`;
+    if (`${month}`.length < 2)
+        month = `0${month}`;
+    if (`${hours}`.length < 2)
+        hours = `0${hours}`;
+    if (`${mins}`.length < 2)
+        month = `0${mins}`;
+
+    if (n <= 1000)
+        return "прямо сейчас";
+    else if (n <= 1000 * 60)
+        return `${Math.floor(n / 1000)} сек. назад`;
+    else if (n <= 1000 * 3600)
+        return `${n / 6e4} мин. назад`;
+    else
+        return `${day}.${month}.${year} ${hours}:${mins}`;
+
+}
