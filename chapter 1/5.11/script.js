4ffb6641c9ce9ff5182fc8b8594ec454fba48e1f
Sergey <graygmai1@gmail.com>
2022-06-21 03:10:14 +0500

    11-6

--- chapter 1/5.11/script.js
+++ chapter 1/5.11/script.js
@@ -94,3 +94,19 @@ function getLastDayOfMonth(year, month) {
 }
 
 console.log(getLastDayOfMonth(2012, 1))
+
+
+/* Напишите функцию getSecondsToday(), возвращающую количество секунд с начала сегодняшнего дня.
+
+Например, если сейчас 10:00, и не было перехода на зимнее/летнее время, то:
+
+getSecondsToday() == 36000 // (3600 * 10)
+Функция должна работать в любой день, т.е. в ней не должно быть конкретного значения сегодняшней даты. */
+
+
+function getSecondsToday() {
+
+    let date = new Date();
+
+    return date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
+}
