09291ed21205354ce53f3a859980a03c9453eadf
Sergey <graygmai1@gmail.com>
2022-06-21 02:15:45 +0500

    9-2

--- chapter 1/5.9/script.js
+++ chapter 1/5.9/script.js
@@ -33,3 +33,29 @@ function sumSalaries(obj) {
 }
 
 console.log(sumSalaries(salaries));
+
+
+
+
+
+/* Напишите функцию count(obj), которая возвращает количество свойств объекта:
+
+let user = {
+  name: 'John',
+  age: 30
+};
+
+alert( count(user) ); // 2
+Постарайтесь сделать код как можно короче.
+
+P.S. Игнорируйте символьные свойства, подсчитывайте только «обычные». */
+
+let user = {
+    name: 'John',
+    age: 30
+};
+
+function count(obj) {
+    return Object.keys(obj).length;
+}
+console.log(count(obj));
