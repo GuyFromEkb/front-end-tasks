fbbb1a63d3ae44f7ff58bbbfe55d74bae5b54444
Sergey <graygmai1@gmail.com>
2022-06-21 02:59:39 +0500

    11-4

--- chapter 1/5.11/script.js
+++ chapter 1/5.11/script.js
@@ -46,3 +46,30 @@ function getLocalDay(date) {
 }
 const date1 = new Date(2012, 0, 3);
 console.log(getLocalDay(date1));
+
+
+
+/* Создайте функцию getDateAgo(date, days), возвращающую число, которое было days дней назад от даты date.
+
+К примеру, если сегодня двадцатое число, то getDateAgo(new Date(), 1) вернёт девятнадцатое и getDateAgo(new Date(), 2) – восемнадцатое.
+
+Функция должна надёжно работать при значении days=365 и больших значениях:
+
+let date = new Date(2015, 0, 2);
+
+alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
+P.S. Функция не должна изменять переданный ей объект date. */
+
+function getDateAgo(date, days) {
+    let dateCopy = new Date(date);
+
+    dateCopy.setDate(date.getDate() - days);
+    return dateCopy.getDate();
+}
+
+let date2 = new Date(2015, 0, 2);
+console.log(getDateAgo(date2, 1)); // 1, (1 Jan 2015)
+console.log(getDateAgo(date2, 2)); // 31, (31 Dec 2014)
+console.log(getDateAgo(date2, 365)); // 2, (2 Jan 2014)
