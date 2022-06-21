c7d8bc77ae7c54a1155a5d3ae17cceb57711ea84
Sergey <graygmai1@gmail.com>
2022-06-21 05:06:36 +0500

    12-2

--- chapter 1/5.12/script.js
+++ chapter 1/5.12/script.js
@@ -15,3 +15,58 @@ let user = {
 const obj = JSON.parse(JSON.stringify(user));
 
 console.log(obj);
+
+
+/* В простых случаях циклических ссылок мы можем исключить свойство, из-за которого они возникают, из сериализации по его имени.
+
+Но иногда мы не можем использовать имя, так как могут быть и другие, нужные, свойства с этим именем во вложенных объектах. Поэтому можно проверять свойство по значению.
+
+Напишите функцию replacer для JSON-преобразования, которая удалит свойства, ссылающиеся на meetup:
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+  place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+
+}));
+
+ в результате должно быть:
+{
+  "title":"Совещание",
+  "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+  "place":{"number":23}
+}
+ */
+
+let room = {
+    number: 23
+};
+
+let meetup = {
+    title: "Совещание",
+    occupiedBy: [{ name: "Иванов" }, { name: "Петров" }],
+    place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+const jsonFile = JSON.stringify(meetup, (key, value) => {
+    // console.log(key, value, );
+    return (key != "" && value == meetup) ? undefined : value;
+});
+
+
+console.log(jsonFile);
