a348b9ea0cce5779ae37f24dda198b0c89a699d0
Sergey <graygmai1@gmail.com>
2022-06-22 00:19:50 +0500

    10-5

--- chapter 1/6.10/script.js
+++ chapter 1/6.10/script.js
@@ -78,9 +78,11 @@ askPassword(user.loginOk, user.loginFail); */
 
 
 function askPassword(ok, fail) {
-    let password = "rockstar";
-    if (password == "rockstar") ok();
-    else fail();
+    let password = "rockstar@@!#!@";
+    if (password == "rockstar")
+        ok();
+    else
+        fail();
 }
 
 let user = {
@@ -96,4 +98,46 @@ let user = {
 
 };
 
-askPassword(user.loginOk.bind(user), user.loginFail);
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+
+/* 
+Это задание является немного усложнённым вариантом одного из предыдущих – Исправьте функцию, теряющую "this".
+Объект user был изменён. Теперь вместо двух функций loginOk/loginFail у него есть только одна – user.login(true/false).
+Что нужно передать в вызов функции askPassword в коде ниже, чтобы она могла вызывать функцию user.login(true) как ok и функцию user.login(false) как fail?
+
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: 'John',
+
+  login(result) {
+    alert( this.name + (result ? ' logged in' : ' failed to log in') );
+  }
+};
+
+askPassword(?, ?); // ?
+Ваши изменения должны затрагивать только выделенный фрагмент кода. */
+
+
+function askPassword(ok, fail) {
+    let password = "rockstar";
+    if (password == "rockstar")
+        ok();
+    else
+        fail();
+}
+
+let user2 = {
+    name: 'John',
+
+    login(result) {
+        console.log(this.name + (result ? ' logged in' : ' failed to log in'));
+    }
+};
+
+askPassword(user2.login.bind(user2, true), user2.login.bind(user2, false));
