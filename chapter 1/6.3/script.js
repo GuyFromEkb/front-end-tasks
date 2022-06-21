d3ad4fa9a0e4bca018387d73dd6b8b6da7b3d16b
Sergey <graygmai1@gmail.com>
2022-06-21 08:53:29 +0500

    3-7

--- chapter 1/6.3/script.js
+++ chapter 1/6.3/script.js
@@ -183,3 +183,57 @@ console.log(users.sort(byField('age')));
 С помощью предыдущего задания, по его шаблону чёто да получилось!!!!
 запомнить бы
 */
+
+
+
+/* 
+Следующий код создаёт массив из стрелков (shooters).
+
+Каждая функция предназначена выводить их порядковые номера. Но что-то пошло не так…
+
+function makeArmy() {
+  let shooters = [];
+
+  let i = 0;
+  while (i < 10) {
+    let shooter = function() { // функция shooter
+      alert( i ); // должна выводить порядковый номер
+    };
+    shooters.push(shooter);
+    i++;
+  }
+
+  return shooters;
+}
+
+let army = makeArmy();
+
+army[0](); // у 0-го стрелка будет номер 10
+army[5](); // и у 5-го стрелка тоже будет номер 10
+// ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3... */
+
+
+function makeArmy() {
+    let shooters = [];
+
+    let i = 0;
+    while (i < 10) {
+
+        let numberOfShooter = i;
+
+        
+        let shooter = function() { 
+            console.log(numberOfShooter); 
+        };
+        shooters.push(shooter);
+        i++;
+    }
+
+    return shooters;
+}
+
+let army = makeArmy();
+
+army[0]();
+army[5]();
+army[7]();
