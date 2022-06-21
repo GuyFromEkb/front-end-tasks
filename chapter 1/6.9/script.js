3ac9544540247e8ce7206dcd5858cefc66716354
Sergey <graygmai1@gmail.com>
2022-06-21 22:29:43 +0500

    9-2

--- chapter 1/6.9/script.js
+++ chapter 1/6.9/script.js
@@ -22,16 +22,49 @@ P.S.: Этот декоратор иногда полезен для юнит-т
 function spy(func) {
 
     function wrapper(...args) {
-      wrapper.calls.push(args);
-      return func.apply(this, arguments);
+        wrapper.calls.push(args);
+        return func.apply(this, arguments);
     }
-  
-    wrapper.calls = [];
-  
-    return wrapper;
-  }
 
-  /* 
-  Очень сложная для меня тема, пользовался ответами
+    wrapper.calls = [];
+
+    return wrapper;
+}
+
+/* 
+Очень сложная для меня тема, пользовался ответами
   
-  */
+*/
+/* 
+Создайте декоратор delay(f, ms), который задерживает каждый вызов f на ms миллисекунд. Например:
+
+function f(x) {
+  alert(x);
+}
+
+// создаём обёртки
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 1500);
+
+f1000("test"); // показывает "test" после 1000 мс
+f1500("test"); // показывает "test" после 1500 мс
+Другими словами, delay(f, ms) возвращает вариант f с «задержкой на ms мс».
+
+В приведённом выше коде f – функция с одним аргументом, но ваше решение должно передавать все аргументы и контекст this. */
+
+
+function delay(f, ms) {
+
+    return function() {
+        setTimeout(() => {
+            f.apply(this, arguments);
+        }, ms);
+    };
+}
+
+let f1000 = delay(console.log, 1000);
+f1000("test"); // показывает "test" после 1000 мс
+
+/* 
+Воспользовался ответами
+*/
