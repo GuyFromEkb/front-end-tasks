539f9aaf04b9474023acc8e660f6b4727f904df0
Sergey <graygmai1@gmail.com>
2022-06-21 06:51:50 +0500

    1-5

--- chapter 1/6.1/script.js
+++ chapter 1/6.1/script.js
@@ -105,4 +105,81 @@ function fib(n) {
 
 }
 
-console.log(fib(7))
+console.log(fib(40))
+
+// UPD Сам дошел только до фибаначи через рекурсию, значение выше 40, уже заставляют процессор долго думать, оптимизированный код подсмотрел в ответах
+
+/* function fib(n) {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  }
+  
+  console.log( fib(3) ); // 2
+  console.log( fib(7) ); // 13
+  console.log( fib(77) ); // 5527939700884757 */
+
+
+/*  Допустим, у нас есть односвязный список (как описано в главе Рекурсия и стек):
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+Напишите функцию printList(list), которая выводит элементы списка по одному.
+
+Сделайте два варианта решения: используя цикл и через рекурсию.
+
+Как лучше: с рекурсией или без? */
+
+
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+
+function printList(list) {
+
+    console.log(list.value)
+
+    if (list.next != null)
+        printList(list.next)
+
+}
+
+printList(list);
+
+function printListv2(list) {
+
+    let valueNow = list;
+
+    while (valueNow != null) {
+        console.log(valueNow.value);
+        valueNow = valueNow.next;
+    }
+
+}
+printListv2(list);
