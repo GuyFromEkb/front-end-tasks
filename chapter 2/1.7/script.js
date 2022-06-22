f6e79f05e153c656a518513697f5a3d6015911de
Sergey <graygmai1@gmail.com>
2022-06-23 04:09:55 +0500

    7-4

--- chapter 2/1.7/script.js
+++ chapter 2/1.7/script.js
@@ -70,5 +70,40 @@ clear(ol);
 
 /* 
 Не правильно написан HTML CODE 
-
 */
+
+
+/* Напишите интерфейс для создания списка.
+
+Для каждого пункта:
+Запрашивайте содержимое пункта у пользователя с помощью prompt.
+Создавайте элемент <li> и добавляйте его к <ul>.
+Продолжайте до тех пор, пока пользователь не отменит ввод (нажатием клавиши Esc или введя пустую строку).
+Все элементы должны создаваться динамически.
+
+Если пользователь вводит HTML-теги, они должны обрабатываться как текст. 
+*/
+
+
+
+function runTask4() {
+
+    const task4 = document.querySelector('.task4');
+    const ulEl = document.createElement('ul');
+
+    task4.append(ulEl);
+
+    while (true) {
+        const text = prompt("содержимое пункта", "");
+
+        if (text != null && text != "") {
+            const newLi = document.createElement('li');
+            newLi.textContent = text;
+            ulEl.appendChild(newLi);
+        } else {
+            break;
+        }
+    }
+}
+
+runTask4();
