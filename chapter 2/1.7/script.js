52801b6bf957c47d0543d6cb9d4eb4092554439a
Sergey <graygmai1@gmail.com>
2022-06-22 08:25:47 +0500

    7-2

--- chapter 2/1.7/script.js
+++ chapter 2/1.7/script.js
@@ -7,9 +7,55 @@ elem.append(document.createTextNode(text))
 elem.innerHTML = text
 elem.textContent = text */
 
+/* 
+1  и 3 
+
+2 сделает text bold
+*/
 
 
 
 
+/* 
+Создайте функцию clear(elem), которая удаляет всё содержимое из elem.
+<ol id="elem">
+  <li>Привет</li>
+  <li>Мир</li>
+</ol>
 
-// const task2 = document.querySelector('.task2');
+<script>
+  function clear(elem) {  }
+
+  clear(elem); // очищает список
+</script>
+
+ */
+
+const task2 = document.querySelector('.task2');
+const ol = task2.querySelector('ol');
+
+// console.dir(ol);
+// console.log(ol.children)
+
+function clear(elem) {
+
+
+    const childs = Array.from(elem.children);
+
+    // childs.forEach(element => {
+    //     console.log(element)
+    // });
+
+    // console.log(childs.length)
+
+
+
+    if (childs.length > 0) {
+        childs.forEach(item => {
+            item.remove();
+        });
+    }
+
+}
+
+clear(ol); // очищает список
