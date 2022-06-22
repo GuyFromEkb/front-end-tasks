cb5a56e67cb8a77be65ab546a7b78ff528edbea2
Sergey <graygmai1@gmail.com>
2022-06-22 06:26:07 +0500

    1-2

--- chapter 1/10.1/script.js
+++ chapter 1/10.1/script.js
@@ -27,5 +27,42 @@ try {
 Есть ли здесь преимущество в использовании finally или оба фрагмента кода одинаковы? Если такое преимущество есть, то дайте пример, когда оно проявляется. */
 
 /* 
-во втором 
+Нужно использовать finally, т.к. если будет throw или return из ф-ции или метода, то дальнейши код в области не сработает...
+finally сработает в любом случае
 */
+
+
+
+
+
+/* Наследование от SyntaxError
+важность: 5
+Создайте класс FormatError, который наследует от встроенного класса SyntaxError.
+Класс должен поддерживать свойства message, name и stack.
+Пример использования:
+
+let err = new FormatError("ошибка форматирования");
+
+alert( err.message ); // ошибка форматирования
+alert( err.name ); // FormatError
+alert( err.stack ); // stack
+
+alert( err instanceof FormatError ); // true
+alert( err instanceof SyntaxError ); // true (потому что наследует от SyntaxError) */
+
+
+
+class FormatError extends SyntaxError {
+    constructor(message) {
+        super(message);
+        this.name = "FormatError";
+    }
+}
+
+const err = new FormatError("ошибка форматирования");
+
+console.log(err.message); // ошибка форматирования
+console.log(err.name); // FormatError
+console.log(err.stack); // stack
+
+console.log(err instanceof SyntaxError); // true
