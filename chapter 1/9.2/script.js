558c20a3294c48a3d0a9f14ccf8954dd0da3b594
Sergey <graygmai1@gmail.com>
2022-06-22 05:04:01 +0500

    9.2

---
+++ chapter 1/9.2/script.js
@@ -0,0 +1,95 @@
+"use strict";
+
+/* В коде ниже класс Rabbit наследует Animal.
+К сожалению, объект класса Rabbit не создаётся. Что не так? Исправьте ошибку.
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    this.name = name;
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
+alert(rabbit.name);
+
+ */
+
+
+class Animal {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+}
+
+class Rabbit extends Animal {
+    constructor(name) {
+        super(name);
+        this.created = Date.now().getHours;
+    }
+}
+
+let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
+console.log(rabbit.name);
+console.log(rabbit.created.getHours());
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+class Clock {
+    constructor({ template }) {
+        this.template = template;
+    }
+
+    render() {
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
+    }
+
+    stop() {
+        clearInterval(this.timer);
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
+    }
+}
+
+
+let clock = new Clock({ template: 'h:m:s' });
+clock.start(); */
