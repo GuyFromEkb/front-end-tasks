33c77635dbb4dda62f29f47883cf80f36b4c4671
Sergey <graygmai1@gmail.com>
2022-06-23 07:32:42 +0500

    9-2

--- chapter 2/1.9/script.js
+++ chapter 2/1.9/script.js
@@ -9,4 +9,21 @@ P.S. Проверьте: если прокрутки нет вообще или 
 const el = document.querySelector('.task1');
 const scrollBottom = el.scrollHeight - el.scrollTop - el.clientHeight;
 
+
 console.log(scrollBottom);
+
+
+
+/* Напишите код, который возвращает ширину стандартной полосы прокрутки.
+Для Windows она обычно колеблется от 12px до 20px. Если браузер не выделяет место под полосу прокрутки (так тоже бывает, она может быть прозрачной над текстом),
+тогда значение может быть 0px.
+P.S. Ваш код должен работать в любом HTML-документе, независимо от его содержимого. */
+
+
+const task2 = document.querySelector('.task2');
+const scrollEl = task2.querySelector('.task2-scroll-el');
+
+const width = scrollEl.offsetWidth - scrollEl.clientWidth - scrollEl.clientLeft;
+
+
+console.log(width);
