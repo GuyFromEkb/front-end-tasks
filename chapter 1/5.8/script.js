79b155658380ac1767536b3814202e396748c98b
Sergey <graygmai1@gmail.com>
2022-06-25 23:14:10 +0500

    8-2

--- chapter 1/5.8/script.js
+++ chapter 1/5.8/script.js
@@ -19,7 +19,7 @@ const messages = [
 
 const weakMap = new WeakMap();
 
-//делаем список не прочитанных сообщений
+//не прочитанных сообщений
 function notRead(arr) {
 
     for (const item of arr) {
@@ -32,14 +32,14 @@ const whoReadMessage = [
     { text: "See you soon", from: "Alice" }
 ];
 
-//меняем статус сообщений на "прочитанный"
+//а тут их прочитали
 function yesRead(arr) {
     for (const item of arr) {
         weakMap.set(item, true);
     }
 }
 
-//проверяем и удаляем из массива сообщений, все прочитанные
+
 function readMessage(arr) {
 
     for (let i = 0; i < arr.length; i++) {
@@ -53,7 +53,105 @@ notRead(messages);
 yesRead(whoReadMessage);
 readMessage(messages);
 
-//проверяем для кого остались не прочитанные сообщения или какие ...
+//остались непрочитанные 
 for (const item of messages) {
     console.log(item.from);
+}
+
+
+
+/* Есть массив сообщений такой же, как и в предыдущем задании.
+
+let messages = [
+  { text: "Hello", from: "John" },
+  { text: "How goes?", from: "John" },
+  { text: "See you soon", from: "Alice" }
+];
+Теперь вопрос стоит так: какую структуру данных вы бы предложили использовать для хранения информации о том, когда сообщение было прочитано?
+В предыдущем задании нам нужно было сохранить только факт прочтения «да или нет». Теперь же нам нужно сохранить дату, и она должна исчезнуть из памяти при удалении «сборщиком мусора» сообщения.
+P.S. Даты в JavaScript можно хранить как объекты встроенного класса Date, которые мы разберём позднее. */
+
+
+let messages1 = [
+    { text: "Hello", from: "John" },
+    { text: "How goes?", from: "John" },
+    { text: "See you soon", from: "Alice" }
+];
+
+
+let weakMap1 = new WeakMap();
+
+//сообщения отправлены но не прочитаны
+function dateMakeMessage(arr) {
+
+    for (const item of arr) {
+        weakMap1.set(item, null);
+    }
+}
+
+//прочитали 2 сообщения
+function dateReadMessage() {
+    weakMap1.set(messages1[1], new Date().getTime() + 1000 * 60);
+    weakMap1.set(messages1[2], new Date().getTime() + 4000 * 60);
+}
+
+
+dateMakeMessage(messages1);
+dateReadMessage();
+
+for (const item of messages1) {
+
+    if (weakMap1.get(item) === null) {
+        console.log(`сообщение "${item.text}" не было прочитано `)
+    } else {
+        console.log(`сообщение "${item.text}" прочитано ${new Date(weakMap1.get(item))}`)
+    }
+
+    console.log(weakMap1.get(item));
+}
+
+
+console.log(weakMap1.get(messages1[1]));
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(new Date(Date.now()))
+/* 
+//допустим эти сообщения прочитали
+let readsMessage = [
+    { text: "Hello", from: "John" },
+    { text: "See you soon", from: "Alice" }
+];
+
+//меняем статус сообщений на "прочитанный"
+function yesReadMessage(arr) {
+    for (let item of arr) {
+        weakMap.set(item, new Date(Date().getTime() + 1000 * 60));
+    };
 };
+
+//проверяем и удаляем из массива сообщений, все прочитанные
+function readMessage(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (weakMap.has(arr[i]))
+            arr.splice(i, 1);
+    };
+};
+
+noReadMessage(messages1);
+yesReadMessage(readsMessage);
+readMessage(messages1);
+
+//проверяем для кого остались не прочитанные сообщения или какие ...
+for (let item of messages1) {
+    console.log(item.from);
+}; */
